7303627d0cc634e9e5b6e7cb4f13caf0724f1831
htran25 <htran25@ucsc.edu>
2021-08-06 21:10:56 -0700

    player vision changes when underwater

--- Assets/Scripts/Player/PlayerVision.cs
+++ Assets/Scripts/Player/PlayerVision.cs
@@ -3,29 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerVision : MonoBehaviour {
-    // Start is called before the first frame update
+    public Transform perspective;
+    public int chunkLayer;
+
+    public Color underwaterFogColor = new Color(0.15f, 0.55f, 0.75f, 0.6f);
+    public float underwaterFogDensity = 0.01f;
+
+    Transform currentWaterTransform;
+    // TerrainMovement currentWaterMovement;
+
+    enum VisionMode { Default, Underwater }; // Might be useful if we ever add other vision effects?
+    VisionMode visionMode = VisionMode.Default;
+
+    // http://wiki.unity3d.com/index.php?title=Underwater_Script
+    bool defaultFog;
+    Color defaultFogColor;
+    float defaultFogDensity;
+    // Material defaultSkybox;
+    // Material noSkybox;
+
+    float waveLength;
+    float waveSpeed;
+    float waveHeight;
+
     void Start() {
-        
+        defaultFog = RenderSettings.fog;
+        defaultFogColor = RenderSettings.fogColor;
+        defaultFogDensity = RenderSettings.fogDensity;
+        // defaultSkybox = RenderSettings.skybox;
     }
 
-    // Update is called once per frame
     void Update() {
-        
+        // Only runs if player is below water level and vision mode has not been switched to type Underwater yet
+        float currentWaterLevel = GetWaveHeightAtPosition(perspective.position);
+        // Debug.Log(perspective.position + " | " + currentWaterLevel);
+        if (perspective.position.y < currentWaterLevel && visionMode == VisionMode.Default) {
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = underwaterFogColor;
+            RenderSettings.fogDensity = underwaterFogDensity;
+
+            visionMode = VisionMode.Underwater;
+        }
+        // Only runs if player is above water level and the vision mode has not been switched to type Default yet
+        else if (perspective.position.y >= currentWaterLevel && visionMode == VisionMode.Underwater) {
+            RenderSettings.fog = defaultFog;
+            RenderSettings.fogColor = defaultFogColor;
+            RenderSettings.fogDensity = defaultFogDensity;
+
+            visionMode = VisionMode.Default;
+        }
     }
 
-    // private void UpdatePlayerVision() {
-    //     if (currentWaterMovementData == null) return;
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == chunkLayer) {
+            currentWaterTransform = other.transform.childCount > 1 ? other.transform.GetChild(1) : null; // NOTE: Assumes that water object is the 2nd child of chunk object
+            if (currentWaterTransform != null) {
+                MeshRenderer currentWaterMeshRenderer = currentWaterTransform.GetComponent<MeshRenderer>();
+                waveLength = currentWaterMeshRenderer.sharedMaterial.GetFloat("_WaveLength");
+                waveSpeed = currentWaterMeshRenderer.sharedMaterial.GetFloat("_WaveSpeed");
+                waveHeight = currentWaterMeshRenderer.sharedMaterial.GetFloat("_WaveHeight");
+            }
+        }
+    }
 
-    //     float waterLevelAtPosition = currentWaterMovementData.GetWaveHeightAtPosition(transform.position);
-    //     if (transform.position.y < waterLevelAtPosition) {
-    //         RenderSettings.fog = true;
-    //         RenderSettings.fogColor = new Color(0f, 0.4f, 0.7f, 0.6f);
-    //         RenderSettings.fogDensity = 0.04f; 
-    //     }
-    //     else {
-    //         RenderSettings.fog = true;
-    //         RenderSettings.fogColor = new Color(0f, 0.4f, 0.7f, 0.6f);
-    //         RenderSettings.fogDensity = 0.04f; 
-    //     }
-    // }
+    private float GetWaveHeightAtPosition(Vector3 position) {
+        // Calculate Gerstner wave movements
+        float p = (position.x + position.z) / 16;
+        float k = 2 * Mathf.PI / waveLength;
+        float f = k * (p - waveSpeed * Shader.GetGlobalVector("_Time").y);
+
+        return currentWaterTransform != null ? currentWaterTransform.position.y + waveHeight * Mathf.Sin(f) : 0;
+    }
 }
