e894752ba0c290e57bec879b11f4fcf3135d11c3
htran25 <htran25@ucsc.edu>
2021-08-21 23:03:45 -0700

    completely rewrote movement mechanics (movement still clunky under certain situations)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,120 +3,164 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     public float movementSpeed = 16f;
     public float jumpHeight = 4f;
-    public float slideSpeed = 25f;
+    public float slideSpeed = 16f;
+    [Range (0, 1)]
+    public float friction = 0.875f;
+
+    public float jumpCooldown = 1f;
 
     public Transform perspective;
     public float sensitivity = 2f;
 
-    public int chunkLayer;
-
     CharacterController playerController;
     Vector3 velocity;
-    Vector3 surfaceNormal;
+    Vector3 directionalInput;
 
-    GameObject currentChunk;
+    bool isGrounded = false;
+
+    Vector3 latestSurfaceContactPoint;
+    Vector3 latestSurfaceNormal;
+    float latestSurfaceNormalAngle;
+
+    Vector3 slideDirection;
+
+    float jumpCooldownRemaining;
 
     float yaw = 0f;
     float pitch = 0f;
 
     void Start() {
-        playerController = GetComponent<CharacterController>();
-        velocity = Vector3.zero;
-        surfaceNormal = Vector3.up;
-
-        // currentChunk = GetCurrentChunk();
-
+        Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        playerController = GetComponent<CharacterController>();
     }
 
     void Update() {
-        // Debug.Log("Grounded?: " + playerController.isGrounded);
+        isGrounded = playerController.isGrounded;
+        HandleCooldownTimers();
+
         MovePlayer();
         RotatePlayer();
     }
 
     void OnControllerColliderHit(ControllerColliderHit hit) {
+        // Debug.DrawRay(transform.position, hit.normal * 3, Color.green);
+        // Debug.Log(hit.moveDirection);
         if (hit.moveDirection == Vector3.down) {
-            // Point on player controller where surface contacts player
-            Vector3 surfaceContactPoint = hit.point;
-            surfaceContactPoint.y += playerController.skinWidth;
-            surfaceNormal = CalculateSurfaceNormal(surfaceContactPoint);
+            // Debug.Log(newSurfaceContactPoint.y + " | " + hit.point.y);
+            UpdateSurfaceData(hit.point);
         }
     }
 
-    void OnTriggerEnter(Collider other) {
-        if (other.gameObject.layer == chunkLayer) {
-            currentChunk = other.gameObject;
-        }
+
+
+    private void HandleCooldownTimers() {
+        // Jumping
+        if (jumpCooldownRemaining > 0f) jumpCooldownRemaining -= Time.deltaTime;
+        if (jumpCooldownRemaining < 0f) jumpCooldownRemaining = 0f;
+        
+        // Other cooldown stuff goes here :)
     }
 
+    [HideInInspector] private float antiFriction;
     private void MovePlayer() {
-        Vector3 localXMovement = Input.GetAxis("Horizontal") * transform.right * movementSpeed;
-        Vector3 localYMovement = CalculateVerticalMovement();
-        Vector3 localZMovement = Input.GetAxis("Vertical") * transform.forward * movementSpeed;
+        antiFriction = 1f - friction;
 
-        velocity = localXMovement + localYMovement + localZMovement;
-
-        /* 
-         * FIXME: 
-         *      1. fix jittery sliding when colliding with multiple planes of differing normals
-         *      2. (FIXED!! yay.. i think..) fix jittery sliding when sliding down VERY STEEP slopes (isGrounded becomes false)
-         */
-        if (playerController.isGrounded) {
-            float normalAngle = Vector3.Angle(transform.up, surfaceNormal);
-            // Debug.Log(normalAngle);
-
-            if (normalAngle > playerController.slopeLimit) {
-                velocity.x += (1f - surfaceNormal.y) * surfaceNormal.x * slideSpeed;
-                velocity.z += (1f - surfaceNormal.y) * surfaceNormal.z * slideSpeed;
-            }
-        }
+        UpdateHorizontalMovement();
+        UpdateVerticalMovement();
 
         playerController.Move(velocity * Time.deltaTime);
     }
 
     private void RotatePlayer() {
-        yaw += Input.GetAxis("Mouse X") * sensitivity;
-        pitch -= Input.GetAxis("Mouse Y") * sensitivity;
-        
-        float maxUpAngle = -90f;
-        float maxDownAngle = 45f;
-        pitch = Mathf.Clamp(pitch, maxUpAngle, maxDownAngle);
+        yaw = ((yaw + Input.GetAxis("Mouse X") * sensitivity) + 360) % 360;
+        pitch = Mathf.Clamp(pitch + Input.GetAxis("Mouse Y") * sensitivity, -45f, 90f);
 
         transform.localEulerAngles = new Vector3(0f, yaw, 0f);
-        perspective.localEulerAngles = new Vector3(pitch, 0f, 0f);
+        perspective.localEulerAngles = new Vector3(-pitch, 0f, 0f);
     }
 
-    private Vector3 CalculateVerticalMovement() {
-        // https://forum.unity.com/threads/controller-isgrounded-doesnt-work-reliably.91436/#post-592670
-        // TODO: Figure out why grounded Y-velocity is set to -stepOffset / deltaTime?
-        if (playerController.isGrounded) {
-            if (Input.GetButtonDown("Jump")) {
-                float jumpForce = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
-                velocity.y = jumpForce;
-            }
-            else {
-                velocity.y = Physics.gravity.y * 4; // -playerController.stepOffset / Time.deltaTime;
-            }
+    [HideInInspector] private Vector3 directionalVelocity;
+    [HideInInspector] private float slidingAntiFriction;
+    private void UpdateHorizontalMovement() {
+        Vector3 targetDirection = (Input.GetAxisRaw("Horizontal") * transform.right) + (Input.GetAxisRaw("Vertical") * transform.forward).normalized;
+        directionalInput = Vector3.SmoothDamp(directionalInput, targetDirection, ref directionalVelocity, antiFriction);
+
+        /* Handles sliding mechanic off of steep surfaces -- balance of numbers is key here, any changes in movementSpeed, slideSpeed, or friction 
+         * (current values: 16, 16, 0.875 -- TODO: figure out relationship formula between the three) will mess up the equilibrium
+         */
+        if (latestSurfaceNormalAngle > playerController.slopeLimit) {
+            slidingAntiFriction = antiFriction * 0.125f;
+
+            velocity.x = (slideDirection.x * slideSpeed) + (directionalInput.x * movementSpeed * slidingAntiFriction);
+            velocity.z = (slideDirection.z * slideSpeed) + (directionalInput.z * movementSpeed * slidingAntiFriction);
         }
         else {
-            velocity.y += Physics.gravity.y * Time.deltaTime;
+            velocity.x = directionalInput.x * movementSpeed;
+            velocity.z = directionalInput.z * movementSpeed;
         }
-
-        return transform.up * velocity.y;
     }
 
-    private Vector3 CalculateSurfaceNormal(Vector3 surfaceContactPoint) {
+    [HideInInspector] private float jumpForce;
+    private void UpdateVerticalMovement() {
+        // TODO: allow players to jump while sliding under certain circumstances
+        if (latestSurfaceNormalAngle > playerController.slopeLimit) {
+            if (isGrounded) velocity.y = 0f;
+            else velocity.y += Physics.gravity.y * Time.deltaTime;
+        }
+        else {
+            if (isGrounded) {
+                if (Input.GetButton("Jump") && jumpCooldownRemaining == 0f) {
+                    jumpForce = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+                    velocity.y = jumpForce;
+                    jumpCooldownRemaining = jumpCooldown;
+                }
+                else {
+                    velocity.y = -movementSpeed;
+                }
+            }
+            else {
+                velocity.y += Physics.gravity.y * Time.deltaTime;
+            }
+        }
+    }
+
+    private void UpdateSurfaceData(Vector3 newSurfaceContactPoint) {
+        if (latestSurfaceContactPoint != newSurfaceContactPoint) {
+            latestSurfaceContactPoint = newSurfaceContactPoint;
+
+            newSurfaceContactPoint.y += playerController.skinWidth;
+            Vector3 newSurfaceNormal = CalculateNormalBelow(newSurfaceContactPoint);
+            float newNormalAngle = Vector3.Angle(Vector3.up, newSurfaceNormal);
+
+            if (latestSurfaceNormal != newSurfaceNormal) {
+                latestSurfaceNormal = newSurfaceNormal;
+                UpdateSlideDirection();
+            }
+
+            if (latestSurfaceNormalAngle != newNormalAngle) {
+                latestSurfaceNormalAngle = newNormalAngle;
+            }
+        }
+    }
+
+    private void UpdateSlideDirection() {
+        slideDirection = Vector3.Cross(latestSurfaceNormal, Vector3.Cross(latestSurfaceNormal, Vector3.up));
+
+        Debug.DrawRay(transform.position, slideDirection * 3, Color.green);
+    }
+
+    private Vector3 CalculateNormalBelow(Vector3 position) {
         RaycastHit hit;
 
-        Ray ray = new Ray(surfaceContactPoint, -transform.up);
-        float rayLength = playerController.skinWidth + 0.1f;
+        Ray ray = new Ray(position, Vector3.down);
+        float rayLength = playerController.height;
+        Debug.DrawRay(position, Vector3.down * rayLength, Color.red);
 
-        bool surfaceDetected = Physics.Raycast(ray, out hit, rayLength);
-        Debug.DrawRay(ray.origin, ray.direction * rayLength, surfaceDetected ? Color.green : Color.red);
-        // Debug.Log("Surface contact point: " + surfaceContactPoint + " | " + surfaceDetected);
+        bool foundSurface = Physics.Raycast(ray, out hit, rayLength);
+        // Debug.Log(foundSurface + " | " + Vector3.Angle(Vector3.up, hit.normal));
 
-        // If no surface detected, return vector that results in 0-degree angle
-        return surfaceDetected ? hit.normal : transform.up;
+        return foundSurface ? hit.normal : Vector3.up;
     }
 }
