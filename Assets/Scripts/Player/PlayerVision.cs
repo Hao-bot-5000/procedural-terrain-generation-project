9315b6135a9002c114fa3744d1856955f0861c52
htran25 <htran25@ucsc.edu>
2021-09-03 01:08:57 -0700

    temporary changes for render distance

--- Assets/Scripts/Player/PlayerVision.cs
+++ Assets/Scripts/Player/PlayerVision.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerVision : MonoBehaviour {
-    public Transform perspective;
+    public Transform playerCamera;
     public int chunkLayer;
+    public int backgroundLayer;
 
+    // TODO: underwater fog color needs to scale with time of day
     public Color underwaterFogColor = new Color(0.15f, 0.55f, 0.75f, 0.6f);
     public float underwaterFogDensity = 0.01f;
 
@@ -31,13 +31,15 @@ public class PlayerVision : MonoBehaviour {
         defaultFogColor = RenderSettings.fogColor;
         defaultFogDensity = RenderSettings.fogDensity;
         // defaultSkybox = RenderSettings.skybox;
+
+        UpdateCullDistances();
     }
 
     void Update() {
         // Only runs if player is below water level and vision mode has not been switched to type Underwater yet
-        float currentWaterLevel = GetWaveHeightAtPosition(perspective.position);
-        // Debug.Log(perspective.position + " | " + currentWaterLevel);
-        if (perspective.position.y < currentWaterLevel && visionMode == VisionMode.Default) {
+        float currentWaterLevel = GetWaveHeightAtPosition(playerCamera.position);
+        // Debug.Log(playerCamera.position + " | " + currentWaterLevel);
+        if (playerCamera.position.y < currentWaterLevel && visionMode == VisionMode.Default) {
             RenderSettings.fog = true;
             RenderSettings.fogColor = underwaterFogColor;
             RenderSettings.fogDensity = underwaterFogDensity;
@@ -45,7 +47,7 @@ public class PlayerVision : MonoBehaviour {
             visionMode = VisionMode.Underwater;
         }
         // Only runs if player is above water level and the vision mode has not been switched to type Default yet
-        else if (perspective.position.y >= currentWaterLevel && visionMode == VisionMode.Underwater) {
+        else if (playerCamera.position.y >= currentWaterLevel && visionMode == VisionMode.Underwater) {
             RenderSettings.fog = defaultFog;
             RenderSettings.fogColor = defaultFogColor;
             RenderSettings.fogDensity = defaultFogDensity;
@@ -64,6 +66,23 @@ public class PlayerVision : MonoBehaviour {
                 waveHeight = currentWaterMeshRenderer.sharedMaterial.GetFloat("_WaveHeight");
             }
         }
+    }
+
+    // NOTE: temporary until I develop my own culling mechanic
+    private void UpdateCullDistances() {
+        Camera cam = playerCamera.GetComponent<Camera>();
+
+        float[] cullDistances = cam.layerCullDistances;
+
+        for (int i = 0; i < cullDistances.Length; i++) {
+            // If layer is not considered a background, reduce cull distance
+            if (i != backgroundLayer) {
+                cullDistances[i] = cam.farClipPlane * 0.85f;
+            }
+        }
+
+        cam.layerCullDistances = cullDistances;
+        cam.layerCullSpherical = true;
     }
 
     private float GetWaveHeightAtPosition(Vector3 position) {
